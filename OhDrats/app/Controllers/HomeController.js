97018b5928a9c1fb404cad6d4da205b6c46135f0
Alicia <akb.miceli@gmail.com>
2017-06-19 20:13:39 -0500

    asd;lfk

--- OhDrats/app/Controllers/HomeController.js
+++ OhDrats/app/Controllers/HomeController.js
@@ -1,18 +1,19 @@
 ﻿app.controller("HomeController", ["$scope", "$http", "$location",
     function ($scope, $http, $location) {
 
-        $scope.logout = function () {
 
-            sessionStorage.removeItem('token');
-            $http.defaults.headers.common['Authorization'] = "";
-
-            $location.path("/login");
-        };
 
         $scope.goLogin = function () {
             $location.path("/login");
         };
 
+<<<<<<< Updated upstream
+        $scope.goLogin = function () {
+            $location.path("/login");
+        };
+
+=======
+>>>>>>> Stashed changes
         $scope.goSignUp = function () {
             $location.path("/signup");
         };
